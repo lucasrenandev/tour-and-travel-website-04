c6b180bafaafe26fd35e270970cfc8db3cb722cd
lucasrenandns <lucasrenandns96@gmail.com>
2023-08-21 12:51:38 -0300

    Updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,17 @@
 const header = document.getElementById("header")
+const menuIcon = document.getElementById("menu-icon")
+const navBar = document.querySelector(".navbar")
 
 window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", this.window.scrollY > 0) 
+})
+
+menuIcon.addEventListener("click", function() {
+    navBar.classList.toggle("open")
+    this.classList.toggle("bx-x")
+})
+
+window.addEventListener("scroll", function() {
+    menuIcon.classList.remove("bx-x")
+    navBar.classList.remove("open")
 })
